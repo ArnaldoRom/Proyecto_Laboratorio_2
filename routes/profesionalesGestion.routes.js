a2686e461e62a82e69236ccf70da569c03dd7018
Matias <ejarquegithub@gmail.com>
2024-10-21 16:11:06 -0300

    rutas
    profesionalesGestion

---
+++ routes/profesionalesGestion.routes.js
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import {
+  crearEspecialidad,
+  obtenerEspecialidades,
+  actualizarEspecialidad,
+  desactivarEspecialidad,
+  activarEspecialidad,
+  crearProfesional,
+  actualizarProfesional,
+  altaProfesional,
+  bajaProfesional,
+  crearProfesionalEspecializado,
+  obtenerProfesionalesEspecializados,
+  actualizarProfesionalEspecializado,
+  eliminarProfesionalEspecializado
+} from "../controllers/profesionalesGestion.controllers.js";
+
+const router = Router();
+
+// Rutas para Especialidades
+router.post("/especialidades", crearEspecialidad);
+router.get("/especialidades", obtenerEspecialidades);
+//router.post("/especialidades/:id/actualizar", actualizarEspecialidad); 
+//router.post("/especialidades/:id/desactivar", desactivarEspecialidad); 
+router.post("/especialidades/:id/activar", activarEspecialidad); 
+
+// Rutas para Profesionales
+router.post("/profesionales", crearProfesional);
+router.post("/profesionales/:id/actualizar", actualizarProfesional); 
+router.post("/profesionales/:id/alta", altaProfesional); 
+router.post("/profesionales/:id/baja", bajaProfesional); 
+
+
+// Rutas para Profesionales Especializados
+router.post("/profesionales-especializados", crearProfesionalEspecializado);
+router.get("/profesionales-especializados", obtenerProfesionalesEspecializados);
+router.post("/profesionales-especializados/:id/actualizar", actualizarProfesionalEspecializado); 
+router.post("/profesionales-especializados/:id/eliminar", eliminarProfesionalEspecializado); 
+
+export default router;
+
