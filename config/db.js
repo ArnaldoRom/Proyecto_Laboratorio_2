4064d34faf2e5a1bbc22dd012ac8a19528c1ca84
Pablo Romero <pablo_5_3_503@hotmail.com>
2024-11-14 20:59:31 -0300

    Faa

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ import { createPool } from "mysql2/promise";
 
 export const conexion = createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: "bmfxiz178dqlzsh7a3u6-mysql.services.clever-cloud.com",
+  user: "ukkqa8nbhmkexhi5",
+  password: "ukkqa8nbhmkexhi5",
   port: 3306,
-  database: "laboratorio",
+  database: "bmfxiz178dqlzsh7a3u6",
 });
