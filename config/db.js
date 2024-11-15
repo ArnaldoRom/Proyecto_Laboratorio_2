ceb801a90db7eb70669a81019c40d96df4140ab5
Pablo Romero <pablo_5_3_503@hotmail.com>
2024-11-14 21:22:35 -0300

    Vamo

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ export const conexion = createPool({
   connectionLimit: 10,
   host: "bmfxiz178dqlzsh7a3u6-mysql.services.clever-cloud.com",
   user: "ukkqa8nbhmkexhi5",
-  password: "ukkqa8nbhmkexhi5",
+  password: "yv7lunKWlQXd172rWSc2",
   port: 3306,
   database: "bmfxiz178dqlzsh7a3u6",
 });
